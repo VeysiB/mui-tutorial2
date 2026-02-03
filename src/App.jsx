2aef3de4451e92840ca53936351403d751f07307
Veysi <veysibegde@gmail.com>
2026-02-03 17:51:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import Navbar from './components/Navbar/Navbar'
+import "@fontsource/roboto"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
